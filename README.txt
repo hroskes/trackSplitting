6b4bccb58a311ce9dbfbf1047f21150808db65e8
whitbeck <>
2012-11-06 02:30:35 +0000

    adding example script for unbinned fits.  updating instructions accordingly.

--- README.txt
+++ README.txt
@@ -1,3 +1,4 @@
+====================
 addDifference.C ----
 ====================
 
@@ -9,3 +10,23 @@ root -l -n
 addDifference("myFile.root")
 
 The output will be save to myFile_wDiffs.root
+
+==================
+unbinnedFit.C ----
+==================
+
+script to plot and fit gaussian to different  variables from 
+the output of the above script.
+
+void unbinnedFits(TString varName="Delta_pt",
+                  double range_low=-10., double range_high=10.,
+                  TString cutString="pt_org>50."){
+
+varName: name of branch to plot/fit
+range_low (high): bounds for plotting/fitting
+cutString: cuts to apply to data before fitting 
+(var names should be the same as the branches in tree)
+
+To run:
+
+root -l -n "unbinnedFits.C(\"Delta_pt\",-2.,2.,\"pt_org>20.&&pt_org<30.\")"
---
+++ unbinnedFits.C
@@ -0,0 +1,40 @@
+
+using namespace RooFit;
+
+void unbinnedFits(TString varName="Delta_pt",
+		  double range_low=-10., double range_high=10.,
+		  TString cutString="pt_org>50."){
+
+  gROOT->ProcessLine(".L ~whitbeck/tdrstyle.C");
+  setTDRStyle();
+
+  RooRealVar x(varName,"#Delta d_{xy}",range_low,range_high);
+  RooRealVar phi("phi_org","#phi",-3.2,0);
+  RooRealVar eta("eta_org","#eta",-2.,2.);
+  RooRealVar pt("pt_org","p_{T}",0.0,1000.0);
+
+  TFile* f =new TFile("TrackSplitting_Split_Alignment_wDiffs.root");
+  TTree* t = (TTree*) f->Get("splitterTree");
+
+  cout << "entries: " << t->GetEntries() << endl;
+
+  RooDataSet data("data","data",t,RooArgSet(x,phi,eta,pt),cutString);
+
+  // initial function and fit
+
+  RooRealVar mean("mean","mean",0.0,-100.0,100.0);
+  RooRealVar width("width","width",1.0,0.,100.0);
+
+  RooGaussian gauss("gauss","gauss",x,mean,width);
+
+  gauss.fitTo(data);
+
+  // plot data and fit
+
+  RooPlot* myPlot = x.frame();
+  data.plotOn(myPlot);
+  gauss.plotOn(myPlot);
+
+  myPlot->Draw();
+
+}
