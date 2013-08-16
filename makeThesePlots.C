32e1ba20005d86cfbf59e96202385a8990b2432e
hroskes <>
2013-08-16 16:05:51 +0000

    more comments

--- makeThesePlots.C
+++ makeThesePlots.C
@@ -14,24 +14,48 @@ TString names[nFiles] = {
 
 TString plotdirectory = "-----directory to save plots-----";
 
+/*******************************
+To make ALL plots (313 in total)
+*******************************/
+
 void makeThesePlots()
 {
     makePlots(nFiles,files,names,"",0,plotdirectory);
 }
 
-//***************************************************************************
-//examples:
-//   xvar = "nHits", yvar = "ptrel" - makes plots of nHits vs Delta_pt/pt_org
-//   xvar = "all",   yvar = "pt"    - makes all plots involving Delta_pt
-//                                    (not Delta_pt/pt_org)
-//   xvar = "",      yvar = "all"   - makes all histograms of Delta_???
-//                                    (including Delta_pt/pt_org)
-//***************************************************************************
+/**************************************************************************
+to make a single plot, or all plots involving a single x or y variable
+
+   xvar = "nHits", yvar = "ptrel" - makes plots of nHits vs Delta_pt/pt_org
+   xvar = "all",   yvar = "pt"    - makes all plots involving Delta_pt
+                                    (not Delta_pt/pt_org)
+   xvar = "",      yvar = "all"   - makes all histograms of Delta_???
+                                    (including Delta_pt/pt_org)
+**************************************************************************/
 
 void makeThesePlots(TString xvar,TString yvar)
 {
     makePlots(nFiles,files,names,"",0,plotdirectory,xvar,yvar);
 }
+
+/*************************************************************************************
+To make a custom selection of plots
+The matrix should look like this - use true to make that plot, and false not to make it.
+        y var   pt rel  pt      eta     phi     dz      dxy     theta   q/pt    (hist)
+x var     +---------------------------------------------------------------------------
+pt        |
+eta       |
+phi       |
+dz        |
+dxy       |
+theta     |
+q/pt      |
+run #     |
+nHits     |
+(hist)    |
+
+*************************************************************************************/
+
 
 void makeThesePlots(Bool_t matrix[xsize][ysize])
 {
